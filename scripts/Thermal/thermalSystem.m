a9058b01f8416838a601bac437d86ced73ed16ba
DraitonT <mdquach@cpp.edu>
2024-05-01 17:02:07 -0700

    Incoporate fins analysis to thermal system analysis

--- scripts/Thermal/thermalSystem.m
+++ scripts/Thermal/thermalSystem.m
@@ -19,6 +19,15 @@ pipe_D = 12.7E-3; % [meter] Diameter of the pipe
 h_convective = 65; % [W/(m^2 * K)]
 T_base = 40; % [C] Temperature of the base 
 
+% Calculate the radiative heat transfer using Newton's Cooling law (Convection)
+width = 1.52; %[m] 
+thickness = 0.01; %[m]
+h_conv = 20; % [W/(m^2K)]
+k_cond = 200; % [W/(m-k)]
+x = .102; % [m] Location of interest
+T_b = 40; % [C] Temperature of base
+T_inf = 20; % [C] Temperature of atmosphere
+
 %%%%%%%%%%%%%%%%%%%%%%%%% EDITABLE %%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%% NON-EDITABLE %%%%%%%%%%%%%%%%%%%%%%%
@@ -35,8 +44,16 @@ surface_area_m2 = 4 * pi * radius_m^2;
 temp_hot_K = temp_hot_C + 273.15;
 temp_cold_K = temp_cold_C + 273.15;
 
-% Calculate the radiative heat transfer using Newton's Cooling law (Convection)
-heat_transfer_radiation = h_convective * surface_area_m2 * (temp_hot_K)
+p = 2 * (width + thickness); % [m] Perimeter 
+A_c = width * thickness; % [m^2] Area of interested
+m = sqrt((h_conv * p)/(k_cond * A_c)); % [1/m]
+
+T_x = exp((-x * m )) * (T_b - T_inf) + T_inf; % [C] Temperature location of interest
+Q_dot_fin = sqrt(h_conv * p * A_c * k_cond) * (T_b - T_inf);
+
+num_fins = 125; % Number of fins
+
+fprintf('The heat transfer done by the fins are %0.4f\n', Q_dot_fin * 125)
 
 % Calculate the radiative heat transfer using Stefan-Boltzmann law (Radiation)
 heat_transfer_W_radiation = emissivity * stefan_boltzmann_constant * surface_area_m2 * ...
